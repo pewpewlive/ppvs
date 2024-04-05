bea549cbe099c254e0470cf317fa0f0e88226833
Mutoxicated <96009711+Mutoxicated@users.noreply.github.com>
2024-04-05 19:56:21 +0300

    fix loading

--- lua/ppvs/init.lua
+++ lua/ppvs/init.lua
@@ -1,9 +1,9 @@
 local ppvs = {}
 
 function ppvs.setup() 
-    local pewpew_funcs = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-funcs.json", false)
-    local pewpew_enums = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-enums.json", false)
-    local fmath_funcs = vim.api.nvim_get_runtime_file("/snippets/generated/fmath-funcs.json", false)
+    local pewpew_funcs = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-funcs.json", false)[1]
+    local pewpew_enums = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-enums.json", false)[1]
+    local fmath_funcs = vim.api.nvim_get_runtime_file("/snippets/generated/fmath-funcs.json", false)[1]
     require("luasnip.loaders.from_vscode").lazy_load({ paths = { pewpew_funcs, pewpew_enums, fmath_funcs }})
 end
 
