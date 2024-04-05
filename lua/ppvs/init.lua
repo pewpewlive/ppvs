7320c2e9ef765857887d09eae247f9333713eece
Mutoxicated <96009711+Mutoxicated@users.noreply.github.com>
2024-04-05 19:50:11 +0300

    test

--- lua/ppvs/init.lua
+++ lua/ppvs/init.lua
@@ -1,10 +1,8 @@
-local pps = require('PewPewSnippets')
-local luasnip = require('LuaSnip')
-
 local ppvs = {}
 
 function ppvs.setup() 
-
+    local file_path = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-funcs.json", false)
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = { })   
 end
 
 return ppvs 
