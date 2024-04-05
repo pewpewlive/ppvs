3046ecd28d606f2127574bc9314e11b34dbb0dd2
Mutoxicated <96009711+Mutoxicated@users.noreply.github.com>
2024-04-05 19:54:03 +0300

    load snippets into luasnips

--- lua/ppvs/init.lua
+++ lua/ppvs/init.lua
@@ -1,8 +1,10 @@
 local ppvs = {}
 
 function ppvs.setup() 
-    local file_path = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-funcs.json", false)
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = { })   
+    local pewpew_funcs = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-funcs.json", false)
+    local pewpew_enums = vim.api.nvim_get_runtime_file("/snippets/generated/pewpew-enums.json", false)
+    local fmath_funcs = vim.api.nvim_get_runtime_file("/snippets/generated/fmath-funcs.json", false)
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = { pewpew_funcs, pewpew_enums, fmath_funcs }})
 end
 
 return ppvs 
